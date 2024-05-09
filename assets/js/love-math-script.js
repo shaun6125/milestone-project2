faf1cfd3148f5bc88d32e31b1da738274ea69b31
shaun craven <craven6622@googlemail.com>
2024-05-09 12:45:12 +0000

    test for script file loading and event handlers working - love math js

--- assets/js/love-math-script.js
+++ assets/js/love-math-script.js
@@ -1,1 +1,53 @@
-console.log("connected")
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("you clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked $(gameType)`);
+            }
+        })
+    }
+})    
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnser() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
