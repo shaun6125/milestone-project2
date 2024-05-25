ec16f4844b8aec18da34ae922aa7b59e61bea2f2
shaun craven <craven6622@googlemail.com>
2024-05-25 23:31:05 +0000

    lights on, show turns functions added and passing testing with jest. 0 added to score div in html, and opacity added to css flash class.

--- assets/js/js-test/simon.test.js
+++ assets/js/js-test/simon.test.js
@@ -3,7 +3,7 @@
  */
 
 
-const { game, newGame, showScore, addTurn } = require("../simon-script");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../simon-script");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -33,6 +33,10 @@ describe("game object contains correct keys", () => {
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["topleft", "topright", "bottomleft", "bottomright"])
     });
+
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
 });
 
 describe("newGame works correctly", () => {
@@ -60,4 +64,40 @@ describe("newGame works correctly", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
 
+    test("expect data-listener to be true", () => {
+        newGame();
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }
+    });
+
 })
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
+    });
+
+     
+});
--- assets/js/simon-script.js
+++ assets/js/simon-script.js
@@ -2,6 +2,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
+    turnNumber: 0,
     choices: ["topleft", "topright", "bottomleft", "bottomright"]
     
 }
@@ -10,6 +11,18 @@ function newGame() {
     game.score = 0;
     game.currentGame = [];
     game.playerMoves = [];
+
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
     addTurn();
 }
@@ -17,13 +30,31 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();
+    showTurns();
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("flash");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove("flash");
+    }, 400);
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 };
 
-module.exports = { game, newGame, showScore, addTurn };
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
 
 
